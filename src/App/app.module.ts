760e283bb1b21a7508dae770f4119d27ec32d8f7
gabrielJGS <gabrielftjose@gmail.com>
2023-07-13 10:51:29 -0300

    feat: import boleto module

--- src/App/app.module.ts
+++ src/App/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { BoletosModule } from 'src/boletos/boletos.module';
 
 @Module({
-  imports: [],
+  imports: [BoletosModule],
   controllers: [AppController],
   providers: [AppService],
 })
