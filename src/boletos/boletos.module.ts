1af201fd84ff786393c8633d5a09f2f8fbdae4b5
gabrielJGS <gabrielftjose@gmail.com>
2023-07-14 15:17:49 -0300

    feat: add lotes and depara providers to module

--- src/boletos/boletos.module.ts
+++ src/boletos/boletos.module.ts
@@ -4,10 +4,17 @@ import { BoletosController } from './boletos.controller';
 import { ConfigModule } from '@nestjs/config';
 import { DatabaseModule } from './db/database.module';
 import { BoletosProviders } from './entities/boleto.providers';
+import { LotesProviders } from './entities/lote.providers';
+import { DeParaProviders } from './entities/depara.providers';
 
 @Module({
   imports: [ConfigModule.forRoot({ isGlobal: true }), DatabaseModule],
   controllers: [BoletosController],
-  providers: [BoletosService, ...BoletosProviders],
+  providers: [
+    BoletosService,
+    ...BoletosProviders,
+    ...LotesProviders,
+    ...DeParaProviders,
+  ],
 })
 export class BoletosModule {}
