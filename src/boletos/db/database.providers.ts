9fa2bba6b6b6d7df7e2d36ab0e0db86060288ca4
gabrielJGS <gabrielftjose@gmail.com>
2023-07-14 15:17:31 -0300

    feat: add lote and depara entites to db provider

--- src/boletos/db/database.providers.ts
+++ src/boletos/db/database.providers.ts
@@ -1,12 +1,14 @@
 import { Sequelize } from 'sequelize-typescript';
 import { Boleto } from '../entities/boleto.entity';
+import { DePara } from '../entities/depara.entity';
+import { Lote } from '../entities/lote.entity';
 
 export const databaseProviders = [
   {
     provide: 'SEQUELIZE',
     useFactory: async () => {
       const sequelize = new Sequelize(process.env.DB_STRING || '');
-      sequelize.addModels([Boleto]);
+      sequelize.addModels([Boleto, Lote, DePara]);
       await sequelize.sync();
       return sequelize;
     },
