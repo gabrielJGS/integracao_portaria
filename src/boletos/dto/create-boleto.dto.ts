9123008dcfec8f089debda33b79b5bbb1c3d7698
gabrielJGS <gabrielftjose@gmail.com>
2023-07-14 22:29:41 -0300

    add apiproperty to createboletodto

--- src/boletos/dto/create-boleto.dto.ts
+++ src/boletos/dto/create-boleto.dto.ts
@@ -1,6 +1,12 @@
+import { ApiProperty } from '@nestjs/swagger';
+
 export class CreateBoletoDto {
+  @ApiProperty()
   nome: string;
+  @ApiProperty()
   unidade: string;
+  @ApiProperty()
   valor: number;
+  @ApiProperty()
   linha_digitavel: string;
 }
