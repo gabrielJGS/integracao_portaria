c0b99a3b2ea8a007af068a7796a2916299b2a1c0
gabrielJGS <gabrielftjose@gmail.com>
2023-07-15 23:28:57 -0300

    fix: boleto module path on app.module

--- src/App/app.module.ts
+++ src/App/app.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { BoletosModule } from 'src/boletos/boletos.module';
+import { BoletosModule } from '../boletos/boletos.module';
 
 @Module({
   imports: [BoletosModule],
